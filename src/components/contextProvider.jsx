70d61e0f9229072f26b7f8b643a8f2a7f57c4ccc
Jimmy Bonilla <jimmyb091202@gmail.com>
2024-02-08 17:15:23 -0800

    url

--- src/components/contextProvider.jsx
+++ src/components/contextProvider.jsx
@@ -6,7 +6,7 @@ const Context = createContext()
 const ContextProvider = ({ children }) => {
     const [session, setSession] = useState({
         header: 'Workouts',
-        API_URL: 'web-production-cffd.up.railway.app',
+        API_URL: 'https://web-production-cffd.up.railway.app',
         user: localStorage.getItem('authTokens') ? jwtDecode(localStorage.getItem('authTokens')).user : null,
         authTokens: localStorage.getItem('authTokens') ? JSON.parse(localStorage.getItem('authTokens')) : null
     })
